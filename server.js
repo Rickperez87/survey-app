9422da8882222dac9e3c4b8f7259c99c1f8d0ae5
Rick Perez <Rickperez87@gmail.com>
2020-12-05 09:36:40 -0500

    configure static serve files

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ let adminId = "";
 let userList = new Set();
 
 const PORT = process.env.PORT || 4000;
-const publicPath = path.join(__dirname, "..", "public");
+const publicPath = path.join(__dirname, "build");
 
 app.use(express.static(publicPath));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(publicPath, "build", "index.html"));
 });
 
 io.on("connect", function (socket) {
