11bbe899629981b2972d31654de89a58ebf85d9b
Rick Perez <Rickperez87@gmail.com>
2021-01-07 17:44:43 -0500

    style surveyResults

--- src/components/surveyResults.js
+++ src/components/surveyResults.js
@@ -3,13 +3,26 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+import { withStyles } from "@material-ui/core/styles";
 
-function SurveyResults({ surveyResults, surveyTitle, open, onClose }) {
+const styles = {
+  root: {
+    width: "500px",
+    margin: "1rem auto",
+    padding: "2rem",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+};
+
+function SurveyResults({ classes, surveyResults, surveyTitle, open, onClose }) {
   return (
     surveyResults && (
-      <Dialog className="container" open={open} onClose={onClose}>
-        <DialogTitle>Survey Results</DialogTitle>
-        <h3>{surveyTitle}</h3>
+      <Dialog className={classes.root} open={open} onClose={onClose}>
+        <DialogTitle className={classes.title}>Survey Results</DialogTitle>
+        <h3 style={{ textAlign: "center" }}>{surveyTitle}</h3>
         <List className="surveyResults-container">
           {surveyResults.map((result, idx) => {
             return (
@@ -24,4 +37,4 @@ function SurveyResults({ surveyResults, surveyTitle, open, onClose }) {
   );
 }
 
-export default SurveyResults;
+export default withStyles(styles)(SurveyResults);
