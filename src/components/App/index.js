5989cf0fc90e4f8437d1fa453d11feca3618242d
Rick Perez <Rickperez87@gmail.com>
2021-01-23 20:09:41 -0500

    set up socketConfig for deploy to heroku

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Survey from "../Survey";
-import LandingPage from "../LandingPage";
+// import LandingPage from "../LandingPage";
 import "./app.css";
 
 const app = () => {
   return (
     <Router>
       <Switch>
+        {/* Landing page is work in progress. not deploying yet */}
+        {/* <Route exact path="/">
+          <LandingPage /> */}
+        {/* </Route> */}
         <Route exact path="/">
-          <LandingPage />
-        </Route>
-        <Route exact path="/app">
           <Survey />
         </Route>
       </Switch>
